3768aa5ad54bcb466cc16a89f0cef6349964d7f7
AntonSlon <mr.kart555@gmail.com>
2025-06-02 01:40:55 +0300

    Update Coordinates.kt

--- src/main/kotlin/org/example/deliveryapplication/model/courier/Coordinates.kt
+++ src/main/kotlin/org/example/deliveryapplication/model/courier/Coordinates.kt
@@ -3,7 +3,6 @@ package org.example.deliveryapplication.model.courier
 import org.springframework.stereotype.Component
 
 
-@Component
 data class Coordinates(
     val longitude: Double = 0.0,
     val latitude: Double = 0.0,
